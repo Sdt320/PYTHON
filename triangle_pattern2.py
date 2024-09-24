01f77f1be81b206b267e8e7193d4b2a2dfc88f4e
Dharshini T <131947889+Sdt320@users.noreply.github.com>
2024-09-24 10:29:20 +0530

    Create triangle_pattern2.py

---
+++ triangle_pattern2.py
@@ -0,0 +1,16 @@
+print("hi welcome to coding")
+'''
+
+* * * * * 
+* * * *
+* * *
+* *
+*
+'''
+
+for x in range(0,5):
+    for j in range(5-x):
+        print("*",end=" ")
+        
+        
+    print()
